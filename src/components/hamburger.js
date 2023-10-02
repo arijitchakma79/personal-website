cf4b2984c8afdb2bdc8b820e04f66bc914021192
Arijit Chakma <ac4393@drexel.edu>
2023-10-01 20:44:28 -0400

    updated gh-pages 6

--- src/components/hamburger.js
+++ src/components/hamburger.js
@@ -1,24 +1,22 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-function HamburgerMenu({name}) {
+function HamburgerMenu({ name, isDarkMode, toggleDarkMode }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isDarkMode, setIsDarkMode] = useState(false);
 
   // Function to toggle the menu
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  // Function to toggle dark mode
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
   // CSS class for the container div
   const containerClass = isMenuOpen
     ? 'relative h-auto bg-gray-900'
-    : 'relative bg-gray-900'; // Set the height to auto when the menu is open
+    : 'relative bg-gray-700'; // Set the height to auto when the menu is open
+
+  // Define the text color classes based on isDarkMode
+  const menuTextColorClass = isDarkMode ? 'text-white' : 'text-gray-800';
+  const darkModeTextColorClass = isDarkMode ? 'text-gray-800' : 'text-white';
 
   const textStyle = {
     fontFamily: "'Mulish', sans-serif",
@@ -29,21 +27,31 @@ function HamburgerMenu({name}) {
       <div className='flex'>
         <button
           onClick={toggleMenu}
-          className="block p-2 text-xl text-gray-300 hover:text-white"
+          className={`block p-2 text-xl text-gray-200 ${menuTextColorClass} hover:text-white`}
         >
           &#9776;
         </button>
         <div className='flex items-center justify-center px-24'>
-            <p style={textStyle} className='text-md text-white'>{name}</p>
+            <p style={textStyle} className={`text-md text-gray-200 ${menuTextColorClass}`}>{name}</p>
+        </div>
+        <div className="flex ml-auto items-center">
+          <div className={`border ${isDarkMode ? 'border-white' : 'border-gray-800'} rounded-md ml-2 px-2`}>
+            <button
+              onClick={toggleDarkMode}
+              className={`block p-2 text-md ${darkModeTextColorClass} hover:text-white`}
+            >
+              {isDarkMode ? 'Light' : 'Dark'}
+            </button>
+          </div>
         </div>
       </div>
       {isMenuOpen && (
-        <div className="fixed w-full bg-gray-900 border-t shadow-md p-2 block">
+        <div className={`fixed w-full ${isDarkMode ? 'bg-gray-900' : 'bg-white'} border-t shadow-md p-2 block`}>
           <ul className="block justify-center text-center">
             <li className="mx-4">
                 <NavLink
                   to="/personal-website/"
-                  className="text-white hover:underline"
+                  className={`text-md ${menuTextColorClass} hover:underline`}
                   onClick={toggleMenu} // Close the menu when a link is clicked
                 >
                   Home
@@ -52,7 +60,7 @@ function HamburgerMenu({name}) {
             <li className="mx-4">
               <NavLink
                 to="/personal-website/projects"
-                className="text-white hover:underline"
+                className={`text-md ${menuTextColorClass} hover:underline`}
                 onClick={toggleMenu} // Close the menu when a link is clicked
               >
                 Projects
@@ -61,7 +69,7 @@ function HamburgerMenu({name}) {
             <li className="mx-4">
               <NavLink
                 to="/personal-website/resume"
-                className="text-white hover:underline"
+                className={`text-md ${menuTextColorClass} hover:underline`}
                 onClick={toggleMenu} // Close the menu when a link is clicked
               >
                 Resume
